0ec5a473de03eb3336bb09fe78b0b74c14fb369b
Benjamin Lee <benjamsandwich@gmail.com>
2016-01-06 23:33:58 -0800

    Parse data for item fields

--- funcheapsf/spiders/funcheapsf_spider.py
+++ funcheapsf/spiders/funcheapsf_spider.py
@@ -1,5 +1,8 @@
 from scrapy.spider import Spider
 from scrapy.selector import Selector
+from scrapy.http.request import Request
+
+from funcheapsf.items import FuncheapsfItem
 
 class FunCheapSFSpider(Spider):
     name = "funcheapsf"
@@ -15,6 +18,42 @@ class FunCheapSFSpider(Spider):
         filename = 'crawled_info'
         with open(filename, 'wb') as f:
             for link in link_elements:
-                f.write(link)
+                item = Request(link, parse2)
+                f.write(item)
 
+
+    def parse2(self, response):
+        sel = Selector(response)
+        item = FuncheapsfItem()
+        item['event'] = Field(sel.css('.title').extract())
+
+        venue = sel.xpath('//b[.="Venue"]/../a/@href').extract()
+        item['venue'] = Field(venue)
+
+        address = sel.xpath('//b[.="Address"]/../text()').extract().split(': ')[1]
+        item['address'] = Field(address) # $x("//b[.='Address']/..")
+
+        day = sel.css('#stats span.left > a').xpath('.//text()').extract()
+        item['day'] = Field(day)
+
+        start_time = sel.css('#stats span.cost').xpath('.//../text()')
+        if start_time.re('All day'):
+            start_time = 'All day'
+        else:
+            start_time = start_time.extract().split(' ')
+            start_time = start_time[2] + ' ' + start_time[3]
+        item['start_time'] = Field(start_time)
+
+        if start_time != 'All day':
+            end_time = sel.css('#stats span.cost').xpath('.//../text()').extract().split(' ')
+            end_time = end_time[-2] + ' ' + end_time[-1]
+            item['end_time'] = Field(end_time)
+
+        cost = sel.xpath('//b[.="Cost:"]/../text()')[0].strip()
+        item['cost'] = Field(cost)
+
+        return item
+
+
+    # TODO: learn why this didn't work
     # sel.css('div#content > div.clearfloat:not(.recurring) div.tanbox.left:first-child')
