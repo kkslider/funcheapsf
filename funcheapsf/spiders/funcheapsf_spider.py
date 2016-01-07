326d565214634b59f2c2988c3eb33fdd7f6764b8
Benjamin Lee <benjamsandwich@gmail.com>
2016-01-06 23:39:11 -0800

    Fix parsing logic for FuncheapsfItem

--- funcheapsf/spiders/funcheapsf_spider.py
+++ funcheapsf/spiders/funcheapsf_spider.py
@@ -16,41 +16,48 @@ class FunCheapSFSpider(Spider):
         sel = Selector(response)
         link_elements = sel.css('div#content > div.clearfloat:not(.recurring) div.tanbox.left span a').xpath('.//@href').extract()
         filename = 'crawled_info'
+        count = 1
+        items = []
         with open(filename, 'wb') as f:
             for link in link_elements:
-                item = Request(link, parse2)
-                f.write(item)
+                # item is a Request object
+                item = Request(link, callback=self.parse2)
+                items.append(item)
+                print count
+                count += 1
+                # return item
+            return items
 
 
     def parse2(self, response):
         sel = Selector(response)
         item = FuncheapsfItem()
-        item['event'] = Field(sel.css('.title').extract())
+        item['event'] = sel.css('.title').xpath('.//text()').extract()[0].split(' | ')[0]
 
-        venue = sel.xpath('//b[.="Venue"]/../a/@href').extract()
-        item['venue'] = Field(venue)
+        venue = sel.xpath('//b[.="Venue"]/../a/text()').extract()[0]
+        item['venue'] = venue
+        address = sel.xpath('//b[.="Address"]/../text()').extract()[0].split(': ')[1]
 
-        address = sel.xpath('//b[.="Address"]/../text()').extract().split(': ')[1]
-        item['address'] = Field(address) # $x("//b[.='Address']/..")
+        item['address'] = address
 
-        day = sel.css('#stats span.left > a').xpath('.//text()').extract()
-        item['day'] = Field(day)
+        day = sel.css('#stats span.left > a').xpath('.//text()').extract()[0]
+        item['day'] = day
 
         start_time = sel.css('#stats span.cost').xpath('.//../text()')
-        if start_time.re('All day'):
-            start_time = 'All day'
+        if len(start_time.re(r'All Day')) > 0:
+            start_time = 'All Day'
         else:
-            start_time = start_time.extract().split(' ')
+            start_time = start_time.extract()[0].split(' ')
             start_time = start_time[2] + ' ' + start_time[3]
-        item['start_time'] = Field(start_time)
+        item['start_time'] = start_time
 
-        if start_time != 'All day':
-            end_time = sel.css('#stats span.cost').xpath('.//../text()').extract().split(' ')
+        if start_time != 'All Day':
+            end_time = sel.css('#stats span.cost').xpath('.//../text()').extract()[0].split(' ')
             end_time = end_time[-2] + ' ' + end_time[-1]
-            item['end_time'] = Field(end_time)
+            item['end_time'] = end_time
 
-        cost = sel.xpath('//b[.="Cost:"]/../text()')[0].strip()
-        item['cost'] = Field(cost)
+        cost = sel.xpath('//b[.="Cost:"]/../text()')[0].extract().strip()
+        item['cost'] = cost
 
         return item
 
